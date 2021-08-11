30f7e05717fcb787d1d88f2603617fdf1a00ee43
Hubert MUGABO <mugberto@gmail.com>
2021-08-11 02:04:44 +0200

    Add calculator markup

---
+++ src/components/Calculator.js
@@ -0,0 +1,28 @@
+function Calculator() {
+  return (
+    <div className="calc-container">
+      <input type="text" value="0" />
+      <button type="button">AC</button>
+      <button type="button">+/-</button>
+      <button type="button">%</button>
+      <button type="button">/</button>
+      <button type="button">7</button>
+      <button type="button">8</button>
+      <button type="button">9</button>
+      <button type="button">*</button>
+      <button type="button">4</button>
+      <button type="button">5</button>
+      <button type="button">6</button>
+      <button type="button">-</button>
+      <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
+      <button type="button">+</button>
+      <button type="button">0</button>
+      <button type="button">.</button>
+      <button type="button">=</button>
+    </div>
+  );
+}
+
+export default Calculator;
