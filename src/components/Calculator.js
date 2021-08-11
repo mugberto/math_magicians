00113c9dd1eecfd6de97156851efe62ff48fc532
Hubert MUGABO <mugberto@gmail.com>
2021-08-11 03:13:36 +0200

    Style Calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,26 +1,28 @@
+import './Calculator.css';
+
 function Calculator() {
   return (
     <div className="calc-container">
       <input type="text" value="0" />
-      <button type="button">AC</button>
-      <button type="button">+/-</button>
-      <button type="button">%</button>
-      <button type="button">/</button>
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">9</button>
-      <button type="button">*</button>
-      <button type="button">4</button>
-      <button type="button">5</button>
-      <button type="button">6</button>
-      <button type="button">-</button>
-      <button type="button">1</button>
-      <button type="button">2</button>
-      <button type="button">3</button>
-      <button type="button">+</button>
-      <button type="button">0</button>
-      <button type="button">.</button>
-      <button type="button">=</button>
+      <button type="button" className="gray-btn">AC</button>
+      <button type="button" className="gray-btn">+/-</button>
+      <button type="button" className="gray-btn">%</button>
+      <button type="button" className="orange-btn">/</button>
+      <button type="button" className="gray-btn">7</button>
+      <button type="button" className="gray-btn">8</button>
+      <button type="button" className="gray-btn">9</button>
+      <button type="button" className="orange-btn">*</button>
+      <button type="button" className="gray-btn">4</button>
+      <button type="button" className="gray-btn">5</button>
+      <button type="button" className="gray-btn">6</button>
+      <button type="button" className="orange-btn">-</button>
+      <button type="button" className="gray-btn">1</button>
+      <button type="button" className="gray-btn">2</button>
+      <button type="button" className="gray-btn">3</button>
+      <button type="button" className="orange-btn">+</button>
+      <button type="button" className="gray-btn">0</button>
+      <button type="button" className="gray-btn">.</button>
+      <button type="button" className="orange-btn">=</button>
     </div>
   );
 }
