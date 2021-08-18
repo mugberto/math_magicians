814aeb1dd3ae158459d9f5bf3321e996168717dd
Hubert MUGABO <mugberto@gmail.com>
2021-08-18 18:26:45 +0200

    Add Quate component

---
+++ src/components/Quote.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Quote() {
+  return (
+    <p>
+      Mathematics is not about numbers, eqations, computations, 
+      or algorithms: it is about understanding. -William Paul Thurston
+    </p>
+  );
+}
