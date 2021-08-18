030fbfc16ce3acc3727a351fd48123be7259cffa
Hubert MUGABO <mugberto@gmail.com>
2021-08-18 18:00:12 +0200

    Add HomePage component

---
+++ src/components/HomePage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <React.Fragment>
+      <h2>Welecome</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Doloremque minus similique impedit iste sunt est consequuntur ratione, 
+        aperiam nobis quidem. 
+        Quo sunt, quia sequi delectus, quaerat ipsa nam hic suscipit. 
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Doloremque minus similique impedit iste sunt est consequuntur ratione, 
+        aperiam nobis quidem. 
+        Quo sunt, quia sequi delectus, quaerat ipsa nam hic suscipit. 
+      </p>
+    </React.Fragment>
+  );
+}
+
+export default HomePage;
