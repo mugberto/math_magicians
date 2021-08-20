7e4b8ab6b450f1424d6c7084cbd940d15d0929d8
Hubert MUGABO <mugberto@gmail.com>
2021-08-20 11:19:20 +0200

    Add test for calculate and isNumber functions

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Should render the brand name', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const Element = screen.getByText(/Math Magicians/i);
+  expect(Element).toBeInTheDocument();
 });
